330086440b124159e32e53450f2ab9e56ccdd4fd
iris3405 <78770213+iris3405@users.noreply.github.com>
2021-04-01 17:48:49 +0900

    Add files via upload

---
+++ FourArithmetic.java
@@ -0,0 +1,24 @@
+public class FourArithmetic {
+    public static void main(String[] args){
+        // int a = 10;
+        // int b = 5;
+        // System.out.println(a+b);
+        // System.out.println(a-b);
+        // System.out.println(a*b);
+        // System.out.println(a/b);
+        // System.out.println();
+        // System.out.println(7 % 3);
+        // System.out.println(3 % 7);
+
+        int i = 0;
+        int j = 0;
+    
+        System.out.println(i++);
+        System.out.println(i);
+        
+
+        System.out.println(i);
+        System.out.println(++j);
+    }
+    
+}
---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        int a = 1;
+        String b ="hello java";
+//주석        
+        System.out.println(a);
+        System.out.println(b);
+    }    
+}
